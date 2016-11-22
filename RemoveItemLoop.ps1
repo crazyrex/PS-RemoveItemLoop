051409a325f199275facd6702241532be67770a3
Tyler Woods <tyler.woods66@gmail.com>
2016-11-22 11:43:25 -0600

    innitial commit

---
+++ RemoveItemLoop.ps1
@@ -0,0 +1,18 @@
+﻿$path = "C:\Users"
+
+Get-ChildItem $path -Directory
+
+# replace 'user' with the prefix for your accounts. 
+# by default, it will look for user1, user2 ... ... user29, user30.
+#
+Function BlastOff {
+Remove-Item $path\user$i\Desktop\'Google Chrome.lnk'
+}
+
+# starting with user1 and ending with user30
+# for($i=1; $i -le 30; $i++)
+
+for($i=1; $i -le 30; $i++)
+    {
+    BlastOff
+    }
